68cebeee64a1d88d394153a1b3e6fefccb82f286
Thanh Nha Phan <85920204+willphan1712000@users.noreply.github.com>
2022-01-23 02:27:59 -0500

    Update FloydDetection.cpp

--- FloydDetection.cpp
+++ FloydDetection.cpp
@@ -1,1 +1,20 @@
-
+int findDuplicate(int array[]) {
+    int slow, fast;
+    slow = array[0];
+    fast = array[0];
+    while (true) {
+        slow = array[slow];
+        fast = array[array[fast]];
+        if (slow == fast) {
+            break;
+        }
+    }
+    
+    // once slow and fast meet, return one of them to the head of the array and name it as pointer 1
+    int ptr1 = array[0], ptr2 = fast;
+    while (ptr1 != ptr2) {
+        ptr1 = array[ptr1];
+        ptr2 = array[ptr2];
+    }
+    return ptr1;
+}
